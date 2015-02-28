666e781c88e2c805ea695df665321b6cbdc0f31b
Maddie Fees <madison.fees90@gmail.com>
2015-02-28 13:12:48 -0700

    starting over on the api so I can learn things

--- angular/apis/etsy/app.js
+++ angular/apis/etsy/app.js
@@ -1,11 +1,3 @@
 (function(){
-    var app = angular.module("etsy", [ ]);
-    app.controller('MainController', [ '$http', function($http){
-        var self = this;
-        $http.jsonp('https://openapi.etsy.com/v2/listings/trending.js?callback=JSON_CALLBACK&api_key=q4ubii6kukovuc0hl2e8myxx')
-        .success(function(data){
-            self.results = data.results;
-        });
-    }]); 
-; 
+    var app = angular.module('tiy-catalog' []);
 })();
