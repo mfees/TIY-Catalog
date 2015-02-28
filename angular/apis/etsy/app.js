b1256c79bbd9d8139dcb64a073e6ce79fec4cf18
Maddie Fees <madison.fees90@gmail.com>
2015-02-28 13:42:30 -0700

    adding api varible to call data in a template later oe

--- angular/apis/etsy/app.js
+++ angular/apis/etsy/app.js
@@ -1,5 +1,20 @@
 (function(){
     var app = angular.module('tiy-catalog', []);
-    app.controller("ProductController", function () {
-    });
+    app.controller("ProductController", [ '$http', function ($http) {
+       https://openapi.etsy.com/v2/listings/trending?api_key=q4ubii6kukovuc0hl2e8myxx&fields=url,title,description
+    }]);
+        
+        var API = {
+            scheme: 'https',
+            host: 'openapi.etsy.com',
+            base: '/v2',
+            path: '/listings/trending',
+            params: {
+                api_key: 'q4ubii6kukovuc0hl2e8myxx',
+                includes: 'MainImage',
+                fields: 'title, description, price, category_path'
+            };
+            
+        var apiTemplate = _.template("${scheme}");    
+        };
 })();
