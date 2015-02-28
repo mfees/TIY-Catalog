2806726969ae8266f9da8cf2eb2f4fb39c456002
Maddie Fees <madison.fees90@gmail.com>
2015-02-28 14:06:53 -0700

    making that url in a variable and it works and it's awesome

--- angular/apis/etsy/app.js
+++ angular/apis/etsy/app.js
@@ -1,8 +1,6 @@
 (function(){
     var app = angular.module('tiy-catalog', []);
     app.controller("ProductController", [ '$http', function ($http) {
-       https://openapi.etsy.com/v2/listings/trending?api_key=q4ubii6kukovuc0hl2e8myxx&fields=url,title,description
-    }]);
         
         var API = {
             scheme: 'https',
@@ -10,11 +8,16 @@
             base: '/v2',
             path: '/listings/trending',
             params: {
+                callback: '.js?callback=JSON_callback',
                 api_key: 'q4ubii6kukovuc0hl2e8myxx',
                 includes: 'MainImage',
-                fields: 'title, description, price, category_path'
-            };
+                fields: 'title,description,price,category_path'
+            }
+            };            
             
-        var apiTemplate = _.template("${scheme}");    
-        };
+        var apiTemplate = _.template("${scheme}://${host}${base}${path}${params.callback}&api_key=${params.api_key}&includes=${params.includes}&fields=${params.fields}");
+        console.log(apiTemplate(API));
+    }]);    
 })();
+
+//https://openapi.etsy.com/v2/listings/trending.js?callback=JSON_callback&api_key=q4ubii6kukovuc0hl2e8myxx&includes=MainImage&fields=url,title,description,price,category_path
