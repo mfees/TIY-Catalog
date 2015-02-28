9048c520cee4f0da5ee97a90dc999b2608687cb2
Maddie Fees <madison.fees90@gmail.com>
2015-02-28 16:12:38 -0700

    ahhhhh, the damn images aren't working

--- angular/apis/etsy/app.js
+++ angular/apis/etsy/app.js
@@ -12,23 +12,47 @@
             api_key: 'q4ubii6kukovuc0hl2e8myxx',
             includes: 'MainImage',
             fields: 'title,description,price,category_path'
-            };            
+            };
+        
         var apiTemplate = _.template("${scheme}://${host}${base}${path}${callback}&api_key=${api_key}&includes=${includes}&fields=${fields}");
         console.log(apiTemplate(API));
     
         $http.jsonp(apiTemplate(API))
         .success(function(data){
             self.trendingProducts = data.results
-            console.log(self.trendingProducts);
+
             
             function productTitle(results) {
                 return _.map(results, 'title');
             };
-            self.title = productTitle(self.trendingProducts)
+            self.title = productTitle(self.trendingProducts);
+            
+            function productDescrition(results) {
+                return _.map(results, 'description');
+            };
+            self.description = productDescrition(self.trendingProducts);
+            
+            function productPrice(results) {
+                return _.map(results, 'price');
+            };
+            self.price = productPrice(self.trendingProducts);
+            
+            function productCategory(results) {
+                return _.map(results, 'category_path');
+            };
+            self.category_path = productCategory(self.trendingProducts);
+            
+            function productImageFull() {
+                return _.map(self.trendingProducts.MainImage, 'url_fullxfull');
+            };
+                
+            self.full = productImageFull();
+            
+//            function any(a,b){
+//                return _.map(a,b)
+//            }
+//            any(self.trendingProducts,'title')
 
         });
     }])    
 })();
-
-//openapi.etsy.com/v2/listings/trending.js?callback=JSON_callback&api_key=q4ubii6kukovuc0hl2e8myxx&includes=MainImage&fields=url,title,description,price,category_path
-//openapi.etsy.com/v2/listings/trending.js?callback=JSON_callback&api_key=q4ubii6kukovuc0hl2e8myxx&includes=MainImage&fields=title,description,price,category_path:1
