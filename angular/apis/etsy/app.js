f0cb4085dad5c0d0ed82966cd04ad015ce1c9873
Maddie Fees <madison.fees90@gmail.com>
2015-02-28 15:02:47 -0700

    started to write the success and failed, we be stuck

--- angular/apis/etsy/app.js
+++ angular/apis/etsy/app.js
@@ -2,6 +2,7 @@
     var app = angular.module('tiy-catalog', []);
     app.controller("ProductController", [ '$http', function ($http) {
         
+        var self = this;
         var API = {
             scheme: 'https',
             host: 'openapi.etsy.com',
@@ -14,10 +15,20 @@
                 fields: 'title,description,price,category_path'
             }
             };            
-            
         var apiTemplate = _.template("${scheme}://${host}${base}${path}${params.callback}&api_key=${params.api_key}&includes=${params.includes}&fields=${params.fields}");
         console.log(apiTemplate(API));
-    }]);    
+        
+//        function productFields(){
+//            
+//        };
+    
+        $http.jsonp(apiTemplate(API))
+        .success(function(data){
+            self.results = data.results
+            console.log(self.results);
+        });
+    }])    
 })();
 
-//https://openapi.etsy.com/v2/listings/trending.js?callback=JSON_callback&api_key=q4ubii6kukovuc0hl2e8myxx&includes=MainImage&fields=url,title,description,price,category_path
+//openapi.etsy.com/v2/listings/trending.js?callback=JSON_callback&api_key=q4ubii6kukovuc0hl2e8myxx&includes=MainImage&fields=url,title,description,price,category_path
+//openapi.etsy.com/v2/listings/trending.js?callback=JSON_callback&api_key=q4ubii6kukovuc0hl2e8myxx&includes=MainImage&fields=title,description,price,category_path:1
